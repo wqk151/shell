c8fb2f5ac3d9b38f65caf68afb7ae9ea05fd1ddc
wqk151 <wqk151@163.com>
2017-10-23 15:01:14 +0800

    Update 北京2-GPS重启-发送端.sh

--- 北京2-GPS重启-发送端.sh
+++ 北京2-GPS重启-发送端.sh
@@ -13,12 +13,12 @@ get_data_line_num
 #echo $data_line_num
 if [ $data_line_num -lt 1 ];then
     echo "data is empty"
-    ssh -p 52523 api-datatang.chinacloudapp.cn "sh /data/sh/beijingImpoTexi2Kafka.sh 2"
+    ssh -p 22 192.168.1.1 "sh /data/sh/beijingImpoTexi2Kafka.sh 2"
     sleep 60 # wait 60s
     get_data_line_num
     if [ $data_line_num -lt 1 ];then
         echo "data also is empty please wait 10min"
-        ssh -p 52523 api-datatang.chinacloudapp.cn "sh /data/sh/beijingImpoTexi2Kafka.sh 600"
+        ssh -p 22 192.168.1.1 "sh /data/sh/beijingImpoTexi2Kafka.sh 600"
      fi
 
 fi
